b716578237324b6601a61706675106267e07b033
Ema5-Zyon <emajoule@hotmail.fr>
2021-05-05 15:56:18 +0200

    Deleting Photos

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { PhotoService } from '../services/photo.service';
+import { Photo, PhotoService } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
 
 
 @Component({
@@ -9,7 +10,7 @@ import { PhotoService } from '../services/photo.service';
 })
 export class Tab2Page {
 
-  constructor(public photoService: PhotoService) { }
+  constructor(public photoService: PhotoService,  public actionSheetController: ActionSheetController) { }
 
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
@@ -17,5 +18,27 @@ export class Tab2Page {
 
   async ngOnInit() {
     await this.photoService.loadSaved();
+  }
+
+  public async showActionSheet(photo: Photo, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
   }
 }
