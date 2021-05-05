c2932fa2311520c92f96e549fdc1dd8a7a16a716
Ema5-Zyon <emajoule@hotmail.fr>
2021-05-05 12:26:15 +0200

    Loading Photos from the Filesystem

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,22 +1,39 @@
 import { Injectable } from '@angular/core';
 
-// First, import Capacitor dependencies and get references to the Camera, Filesystem, and Storage plugins:
-import { Plugins, CameraResultType, Capacitor, FilesystemDirectory, 
-  CameraPhoto, CameraSource } from '@capacitor/core';
-  const { Camera, Filesystem, Storage } = Plugins;
-
+// Import Capacitor dependencies and get references to the Camera, Filesystem, and Storage plugins
+import { Plugins, CameraResultType, Capacitor, FilesystemDirectory,
+         CameraPhoto, CameraSource } from '@capacitor/core';
+const { Camera, Filesystem, Storage } = Plugins;
 
 @Injectable({
   providedIn: 'root'
 })
-export class PhotoService {
 
+export class PhotoService {
   public photos: Photo[] = [];
+  private PHOTO_STORAGE: string = "photos";
 
   constructor() { }
-  
+
+  public async addNewToGallery() {
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri, // file-based data; provides best performance
+      source: CameraSource.Camera, // automatically take a new photo with the camera
+      quality: 100 // highest quality (0 to 100)
+    });
+
+    // Save the picture and add it to photo collection
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    this.photos.unshift(savedImageFile);
+
+    Storage.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos)
+    });
+  }
+
   private async savePicture(cameraPhoto: CameraPhoto) {
-    
     // Convert photo to base64 format, required by Filesystem API to save
     const base64Data = await this.readAsBase64(cameraPhoto);
 
@@ -40,10 +57,10 @@ export class PhotoService {
     // Fetch the photo, read as a blob, then convert to base64 format
     const response = await fetch(cameraPhoto.webPath!);
     const blob = await response.blob();
-  
-    return await this.convertBlobToBase64(blob) as string;  
+
+    return await this.convertBlobToBase64(blob) as string;
   }
-  
+
   convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
     const reader = new FileReader;
     reader.onerror = reject;
@@ -53,19 +70,23 @@ export class PhotoService {
     reader.readAsDataURL(blob);
   });
 
-  public async addNewToGallery() {
-    // Take a photo
-    const capturedPhoto = await Camera.getPhoto({
-      resultType: CameraResultType.Uri,  // file-based data; provides best performance
-      source: CameraSource.Camera, // automatically take a new photo with the camera
-      quality: 100 // highest quality (0 to 100)
-    });
+  public async loadSaved() {
+    // Retrieve cached photo array data
+    const photoList = await Storage.get({ key: this.PHOTO_STORAGE });
+    this.photos = JSON.parse(photoList.value) || [];
 
-    // Save the picture and add it to photo collection
-    const savedImageFile = await this.savePicture(capturedPhoto);
-    this.photos.unshift(savedImageFile);
+    // Display the photo by reading into base64 format
+    for (let photo of this.photos) {
+      // Read each saved photo's data from the Filesystem
+      const readFile = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: FilesystemDirectory.Data
+      });
+
+    // Web platform only: Load the photo as base64 data
+    photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+    }
   }
-  
 }
 
 export interface Photo {
